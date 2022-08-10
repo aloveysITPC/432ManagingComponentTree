2744cb2f14208025bcd50709b1e9e40b5337e99c
Ali Loveys <92459028+aloveysITPC@users.noreply.github.com>
2022-08-10 09:49:08 -0400

    updated

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -4,6 +4,7 @@ function ToDoItem(props) {
   //  get props from app.jsx  pass a function onChecked triggered - passed to child
   return (
     <div
+      // returns the item with the id
       onClick={() => {
         props.onChecked(props.id);
       }}
